ff9d5ab3b99d98288fa93442ee43b6f733ff68d1
dGrammatiko <d.grammatiko@gmail.com>
2020-11-11 03:40:53 +0100

    bundle it

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,20 +1,52 @@
+
+// const resolve = require('@rollup/plugin-node-resolve').default
+
+const mount = {
+  src: {url: "/_dist_"},
+  public: {url: "/", static: true, resolve: true}
+};
+
+const proxy = {};
+
+const plugins = [
+  ["@snowpack/plugin-babel"],
+  [
+    "snowpack-plugin-rollup-bundle",
+    {
+      emitHtmlFiles: true,
+      preserveSourceFiles: true,
+      entrypoints: "build/_dist_/index.js"
+    },
+  ],
+];
+
+const installOptions = {
+  NODE_ENV: true,
+  rollup: {
+    plugins: [
+      // resolve(),
+    ]
+  }
+};
+
+const alias = {};
+
+const devOptions = {
+  out: "build",
+  open: "none",
+  bundle: true,
+};
+
+const buildOptions = {
+  clean: true,
+};
+
 module.exports = {
-  "mount": {
-    "src": {url: "/_dist_"},
-    "public": {url: "/", static: true, resolve: true}
-  },
-  plugins: [
-  ],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
+  mount,
+  alias,
+  proxy,
+  plugins,
+  installOptions,
+  devOptions,
+  buildOptions,
 };
